a13618f31bbfd5a128e21f4c2cc27266fb8f9fe0
Magicking <s@6120.eu>
2018-01-03 07:52:35 +0100

    Add basic relay node

---
+++ README.md
@@ -0,0 +1,8 @@
+### Node relay
+
+Simple ethereum relay node without security set to rinkeby
+
+## Environments
+
+ * SECRET:       Ethstats password
+ * CORS_DOMAIN:  Cross-Origin Resource Sharing, default to *
---
+++ relay/run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+DATA_DIR=/data
+ETHSTATS_NAME=ethstats
+gethcmd="/usr/local/bin/geth --datadir "${DATA_DIR}" --cache 512"
+
+if [ x"${CORS_DOMAIN}" == x ]; then
+  CORS_DOMAIN=*
+fi
+
+ethstatsip=`dig +short "${ETHSTATS_NAME}" | head -n 1`
+
+if [ x"${ethstatsip}" != "x" ] && [ x"${WS_SECRET}" != "x" ]; then
+  echo "Reporting stats to ${ethstatsip}"
+  statsopts="--ethstats ${HOSTNAME}:${WS_SECRET}@${ethstatsip}:3000"
+else
+  echo "Not reporting stats, ethstats not present or WS_SECRET not set"
+fi
+
+modules="eth,web3,net"
+
+exec $gethcmd \
+ --fast \
+ --rinkeby \
+ --maxpeers 100 \
+ ${statsopts} \
+ --ws \
+ --wsaddr "0.0.0.0" \
+ --wsapi "${modules}" \
+ --wsorigins "${CORS_DOMAIN}"
